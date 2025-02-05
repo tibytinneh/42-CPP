29dcba28dc732dadf836bc3e68016995fa1a3da6
uwu <55824963+tibytinneh@users.noreply.github.com>
2025-02-05 18:26:50 +0800

    completed ex06 harlFilter

---
+++ module01/ex06/Harl.cpp
@@ -0,0 +1,77 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+Harl::~Harl() {}
+
+void Harl::debug(void)
+{
+	std::cout << C_BLUE "[ DEBUG ]\nI love having extra bacon for my "
+						"7XL-double-cheese-triple-pickle-special-ketchup burger. "
+						"I really do!\n"
+			  << C_DEFAULT << std::endl;
+}
+
+void Harl::info(void)
+{
+	std::cout << C_YELLOW << "[ INFO ]\nI cannot believe adding extra bacon costs more money. "
+							 "You didn’t put enough bacon in my burger! If you did, "
+							 "I wouldn’t be asking for more!\n"
+			  << C_DEFAULT << std::endl;
+}
+
+void Harl::warning(void)
+{
+	std::cout << C_RED << "[ WARNING ]\nI think I deserve to have some extra bacon for free. "
+						  "I’ve been coming for years whereas you started working here "
+						  "since last month.\n"
+			  << C_DEFAULT << std::endl;
+}
+
+void Harl::error(void)
+{
+	std::cout << C_GREEN << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n"
+			  << C_DEFAULT << std::endl;
+}
+
+void Harl::complain(void)
+{
+	Harl::complain("");
+}
+void Harl::complain(std::string level)
+{
+	int index = -1;
+
+	std::string levels[] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			index = i;
+			break;
+		}
+	}
+
+	switch (index)
+	{
+	case 0:
+		debug();
+		// fallthrough
+	case 1:
+		info();
+		// fallthrough
+	case 2:
+		warning();
+		// fallthrough
+	case 3:
+		error();
+		break;
+	default:
+		std::cout << "No idea what the complaint level: \"" << level << "\" is.\n"
+				  << std::endl;
+	}
+}
