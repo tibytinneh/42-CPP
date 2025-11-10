da7c427ca601662dc095cbe6a003752a008aed65
Amanda Ang Shu Qing <aang-shu@c2r6s2.42singapore.sg>
2025-11-10 15:39:42 +0800

    cpp09/ex02: updated main.cpp to reflect pdf

--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -1,32 +1,29 @@
-#include "PmergeMe.hpp"
-#include <iostream>
+#include <sys/time.h>
+
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
+#include <iostream>
 
-#include <sys/time.h>
-static inline double wall_us()
-{
+#include "PmergeMe.hpp"
+static inline double wall_us() {
     struct timeval tv;
     gettimeofday(&tv, 0);
     return tv.tv_sec * 1e6 + tv.tv_usec;
 }
 
-int main(int ac, char **av)
-{
-    if (ac < 2)
-    {
+int main(int ac, char** av) {
+    if (ac < 2) {
         std::cerr << "Error: insufficient arguments." << std::endl;
         return (EXIT_FAILURE);
     }
 
     PmergeMe pm;
 
-    try
-    {
+    try {
         pm.buildContainers(ac, av);
         pm.printVector("Before:", pm.getVector());
-        double v0 = wall_us(); // wall-clock microseconds.
+        double v0 = wall_us();  // wall-clock microseconds.
         pm.sort_vector(/*trace=*/false);
         double v1 = wall_us();
         double vec_us = v1 - v0;
@@ -34,17 +31,15 @@ int main(int ac, char **av)
         pm.sort_deque(/*trace=*/false);
         double d1 = wall_us();
         double deq_us = d1 - d0;
-        pm.printVector("After(v):", pm.getVector());
-        pm.printDeque("After(d):", pm.getDeque());
+        pm.printVector("After: ", pm.getVector());
+        //        pm.printDeque("After(d):", pm.getDeque());
         std::cout.setf(std::ios::fixed);
         std::cout << std::setprecision(5);
         std::cout << "Time to process a range of " << pm.size()
                   << " elements with std::vector : " << vec_us << " us\n";
         std::cout << "Time to process a range of " << pm.size()
                   << " elements with std::deque  : " << deq_us << " us\n";
-    }
-    catch (const std::exception &e)
-    {
+    } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
